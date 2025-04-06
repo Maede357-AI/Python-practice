a1c83f1617c7af422b509d2535b865f88246c141
Maede Khademi Nori <maedehkhademinori.ai@gmail.com>
2025-04-06 17:09:50 +0330

    Added Greetings.py

---
+++ Greetings.py
@@ -0,0 +1,10 @@
+names = ['Maedeh','Raha','Mohana','Mobina']
+message1 = f" Hi {names[0]}, have a nice day."
+message2 = f" Hi {names[1]}, have a nice day."
+message3 = f" Hi {names[2]}, have a nice day."
+message4 = f" HI {names[3]}, have a nice day."
+
+print(message1)
+print(message2)
+print(message3)
+print(message4)
